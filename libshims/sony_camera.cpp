d40a4cc3764474451c902b909c69bc384a238f64
Roger Ortiz <39171306+R0rt1z2@users.noreply.github.com>
2020-03-03 08:13:52 +0100

    pine: camera: fix sony cam fix.

--- libshims/sony_camera.cpp
+++ libshims/sony_camera.cpp
@@ -3,15 +3,15 @@
 
 extern "C" {
    /* Sony Camera Related Symbols */
-   void _ZN7android19MtkCameraParameters19SONY_SCENE_MODE_PET
-   void _ZN7android19MtkCameraParameters19SONY_SCENE_MODE_BABY
-   void _ZN7android16CameraParameters19SONY_SCENE_MODE_PETE
-   void _ZN7android16CameraParameters19SONY_SCENE_MODE_DARK
-   void _ZN7android16CameraParameters19SONY_SCENE_MODE_BACKLIGHT
-   void _ZN7android16CameraParameters19SONY_SCENE_MODE_DISH
-   void _ZN7android16CameraParameters19SONY_SCENE_MODE_DOCUMENT
-   void _ZN7android16CameraParameters19SONY_SCENE_MODE_SOFT_SKIN
-   void _ZN7android16CameraParameters19SONY_SCENE_MODE_SOFT_SNAP
-   void _ZN7android16CameraParameters19SONY_SCENE_MODE_SPOT_LIGHT
-   void _ZN7android16CameraParameters19SONY_SCENE_MODE_SWEEP_STITCH
+   void _ZN7android19MtkCameraParameters19SONY_SCENE_MODE_PET(void);
+   void _ZN7android19MtkCameraParameters19SONY_SCENE_MODE_BABY(void);
+   void _ZN7android16CameraParameters19SONY_SCENE_MODE_PETE(void);
+   void _ZN7android16CameraParameters19SONY_SCENE_MODE_DARK(void);
+   void _ZN7android16CameraParameters19SONY_SCENE_MODE_BACKLIGHT(void);
+   void _ZN7android16CameraParameters19SONY_SCENE_MODE_DISH(void);
+   void _ZN7android16CameraParameters19SONY_SCENE_MODE_DOCUMENT(void);
+   void _ZN7android16CameraParameters19SONY_SCENE_MODE_SOFT_SKIN(void);
+   void _ZN7android16CameraParameters19SONY_SCENE_MODE_SOFT_SNAP(void);
+   void _ZN7android16CameraParameters19SONY_SCENE_MODE_SPOT_LIGHT(void);
+   void _ZN7android16CameraParameters19SONY_SCENE_MODE_SWEEP_STITCH(void);
 }
