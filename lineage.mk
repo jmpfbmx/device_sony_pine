10cf3570cbbb1b51d788e2ba4e19aa041097bf5f
jmpfbmx <42691741+jmpfbmx@users.noreply.github.com>
2020-03-03 10:29:37 +0100

    pine: Restructure lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -19,15 +19,11 @@
 PRODUCT_RELEASE_NAME := Xperia L1
 
 # Inherit some common Lineage stuff.
-#$(call inherit-product, vendor/cm/config/gsm.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/sony/pine/device_pine.mk)
 
-
-## Device identifier. This must come after all inclusions
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := SONY
 PRODUCT_DEVICE := pine
@@ -37,11 +33,6 @@ PRODUCT_MODEL := G3311
 PRODUCT_BOARD := pine
 PRODUCT_MANUFACTURER := sony
 
-PRODUCT_DEFAULT_LANGUAGE := zh
-PRODUCT_DEFAULT_REGION   := CN
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.timezone=Asia/Shanghai
-
 PRODUCT_GMS_CLIENTID_BASE := android-sonymobile
 
 # Set product device & name
@@ -49,4 +40,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
    PRODUCT_NAME=pine \
    BUILD_FINGERPRINT=alps/pine/pine:7.0/2.92.J.7.97_201904232207/1556033626:user/test-keys \
    PRIVATE_BUILD_DESC="pine-user 7.0 2.92.J.7.97_201904232207 1556033626 test-keys"
-
